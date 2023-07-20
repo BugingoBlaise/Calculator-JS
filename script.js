fed08ffe76e513ac59c482217d034877ed68d328
BugingoBlaise <blaisemugisha1@gmail.com>
2023-07-20 23:58:13 +0200

    Good

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// script.js
+
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
@@ -70,9 +72,11 @@ const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equal]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
+
 const previousOperandTextElement = document.querySelector(
   "[data-previous-operand]"
 );
+
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
